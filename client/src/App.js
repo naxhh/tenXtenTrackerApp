7aa07f6592c4ff0ab873506db142e2183aedff07
Ignacio Tolstoy <arkantos798@gmail.com>
2017-02-19 01:56:42 +0100

    First component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import BggUsername from './components/BggUsername'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div id="header">
+            <h1>10x10 Challenge Tracker!</h1>
+        </div>
+        <BggUsername />
       </div>
-    );
+    )
   }
 }
 
---
+++ client/src/components/BggUsername.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class BggUsername extends Component {
+    render() {
+        return (
+            <div id="bgg-username">
+            <label>BGG Username</label>
+            <input type="text" />
+            </div>
+        )
+    }
+}
