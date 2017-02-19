113af491e32bbd10d0d98630c9d3ed786d5b5be3
Ignacio Tolstoy <arkantos798@gmail.com>
2017-02-19 01:41:43 +0100

    Server methods

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const path = require('path');
+const path = require('path')
+const bgg = require('bgg')({timeout: 10000})
 
 //const fs = require('fs')
 //const sqlite = require('sql.js')
@@ -17,9 +18,39 @@ app.get('/', (req, res) => {
 
 app.get('/api/hot', (req, res) => {
     // const param.query.q
+    bgg('hot', {'type': 'boardgame'})
+    .then((r) => res.json(r))
+    .catch(error => res.status(500).json({"mec": error}))
+})
 
-    // fetch url etc..
-    res.json({'test': 'hi'})
+app.get('/api/collection', (req, res) => {
+    bgg('collection', {
+        'subtype': 'boardgame',
+        'excludesubtype': 'boardgameexpansion',
+        'username': req.query.username
+    })
+    .then((r) => {
+        // Query has been queued
+        if (!!r.message) {
+            res.status(202).json(r)
+        } else {
+            res.json(r)
+        }
+    })
+    .catch(error => res.status(500).json({"mec": error}))
+})
+
+app.get('/api/plays', (req, res) => {
+    const date = new Date()
+    bgg('plays', {
+        'subtype': 'boardgame',
+        'mindate': `${date.getFullYear()}-01-01`,
+        'maxdate': `${date.getFullYear()}-12-31`,
+        'username': req.query.username,
+        'id': req.query.id
+    })
+    .then((r) => res.json(r))
+    .catch(error => res.status(500).json({"mec": error}))
 })
 
 app.listen(app.get('port'), () => {
